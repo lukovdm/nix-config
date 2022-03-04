47a113dbcdbffe5cb2b3a587a0dc55a4576ca1c0
Luko van der Maas <me@luko.dev>
2022-03-04 12:15:44 +0100

    :add home manager to flake

--- flake.nix
+++ flake.nix
@@ -2,14 +2,36 @@
   description = "Luko's system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs";
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    home-manager.url = "github:nix-community/home-manager/release-21.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs, ... }@attrs: {
-    nixosConfigurations.nixvm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      specialArgs = attrs;
-      modules = [ ./configuration.nix ];
+  outputs = { self, nixpkgs, home-manager, ... }: 
+  let
+    system = "x86_64-linux";
+
+    pkgs = import nixpkgs {
+      inherit system;
+      config = { allowUnfree = true; };
+    };
+  in {
+    homeManagerConfigurations = {
+      luko = home-manager.lib.homeManagerConfiguration {
+        inherit system pkgs;
+        username = "luko";
+        homeDirectory = "/home/luko";
+        configuration = {
+          imports = [ ./home.nix ];
+        };
+      };
+    };
+
+    nixosConfigurations = {
+      nixvm = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./configuration.nix ];
+      };
     };
   };
 }
