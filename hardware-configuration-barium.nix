d6cb8bbf98a6560387299f5eb68bc5ab9456afb9
Luko van der Maas <me@luko.dev>
2022-09-24 21:30:09 +0200

    vlc settings

--- hardware-configuration-barium.nix
+++ hardware-configuration-barium.nix
@@ -71,4 +71,18 @@
   services.fprintd.enable = true;
   services.xserver.libinput.enable = true;
   hardware.bluetooth.enable = true;
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    vaapiIntel = pkgs.vaapiIntel.override { enableHybridCodec = true; };
+  };
+  
+  hardware.opengl = {
+    enable = true;
+    extraPackages = with pkgs; [
+      intel-media-driver # LIBVA_DRIVER_NAME=iHD
+      vaapiIntel         # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
+      vaapiVdpau
+      libvdpau-va-gl
+    ];
+  };
 }
