aa97f60a0171ec57712b904c857e2e59d84b231e
Luko van der Maas <me@luko.dev>
2022-10-28 15:53:51 +0200

    small fixes

--- outputs/system-conf.nix
+++ outputs/system-conf.nix
@@ -3,16 +3,27 @@
   nixvm = inputs.nixpkgs.lib.nixosSystem {
     inherit system;
     specialArgs = { inherit inputs; };
-    modules = [ ../system/configuration.nix ../system/hardware-configuration/vm.nix ];
+    modules = [ 
+      ../system/configuration.nix 
+      ../system/hardware-configuration/vm.nix 
+    ];
   };
+
   krypton = inputs.nixpkgs.lib.nixosSystem {
     inherit system;
     specialArgs = { inherit inputs; };
-    modules = [ ../system/configuration.nix ../system/hardware-configuration/krypton.nix ];
+    modules = [ 
+      ../system/configuration.nix 
+      ../system/hardware-configuration/krypton.nix
+    ];
   };
+
   barium = inputs.nixpkgs.lib.nixosSystem {
     inherit system;
     specialArgs = { inherit inputs; };
-    modules = [ ../system/configuration.nix ../system/hardware-configuration/barium.nix ];
+    modules = [ 
+      ../system/configuration.nix 
+      ../system/hardware-configuration/barium.nix
+    ];
   };
 }
