4b221d0a5723c945ad280889130ce970eea1f377
Luko van der Maas <me@luko.dev>
2023-05-07 22:26:06 +0200

    newer kernel

--- system/hardware-configurations/xenon.nix
+++ system/hardware-configurations/xenon.nix
@@ -11,6 +11,7 @@
   boot.initrd.availableKernelModules = [ "xhci_pci" "nvme" "uas" "sd_mod" "rtsx_pci_sdmmc" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
+  boot.kernelPackages = pkgs.linuxPackages_latest;
   boot.extraModulePackages = [ ];
 
   # Bootloader.
