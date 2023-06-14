3ea9fded97f6d8bbd420d24bc23a334a659e6f60
Luko van der Maas <me@luko.dev>
2023-06-14 15:52:54 +0200

    add darktable

--- home/home.nix
+++ home/home.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 let
   tex = (pkgs.texlive.combine {
     inherit (pkgs.texlive) scheme-full
@@ -7,16 +7,14 @@ let
 
   pycharm-professional = pkgs.unstable.jetbrains.pycharm-professional.overrideAttrs
     (attrs: {
-      nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ autoPatchelfHook ];
-
-      buildInputs = (attrs.buildInputs or [ ]) ++ [
+      buildInputs = (attrs.buildInputs or [ ]) ++ (with pkgs; [
         stdenv.cc.cc.lib
         libsecret
         e2fsprogs
         libnotify
         linux-pam
         audit
-      ];
+      ]);
 
       # NOTE(Sem Mulder): Hacky way to disable LD_LIBRARY_PATH.
       installPhase = lib.replaceStrings [ "LD_LIBRARY_PATH" ] [ "WRONG_LD_LIBRARY_PATH" ] attrs.installPhase;
@@ -24,16 +22,14 @@ let
 
   webstorm = pkgs.unstable.jetbrains.webstorm.overrideAttrs
     (attrs: {
-      nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ autoPatchelfHook ];
-
-      buildInputs = (attrs.buildInputs or [ ]) ++ [
+      buildInputs = (attrs.buildInputs or [ ]) ++ (with pkgs; [
         stdenv.cc.cc.lib
         libsecret
         e2fsprogs
         libnotify
         linux-pam
         audit
-      ];
+      ]);
 
       # NOTE(Sem Mulder): Hacky way to disable LD_LIBRARY_PATH.
       installPhase = lib.replaceStrings [ "LD_LIBRARY_PATH" ] [ "WRONG_LD_LIBRARY_PATH" ] attrs.installPhase;
@@ -41,16 +37,14 @@ let
 
   clion = pkgs.unstable.jetbrains.clion.overrideAttrs
     (attrs: {
-      nativeBuildInputs = (attrs.nativeBuildInputs or [ ]) ++ [ autoPatchelfHook ];
-
-      buildInputs = (attrs.buildInputs or [ ]) ++ [
+      buildInputs = (attrs.buildInputs or [ ]) ++ (with pkgs; [
         stdenv.cc.cc.lib
         libsecret
         e2fsprogs
         libnotify
         linux-pam
         audit
-      ];
+      ]);
 
       # NOTE(Sem Mulder): Hacky way to disable LD_LIBRARY_PATH.
       installPhase = lib.replaceStrings [ "LD_LIBRARY_PATH" ] [ "WRONG_LD_LIBRARY_PATH" ] attrs.installPhase;
@@ -95,6 +89,7 @@ in
       audacity
       blender
       obsidian
+      darktable
 
       # Programming stuff
       coq
