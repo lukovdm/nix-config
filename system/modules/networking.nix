b2dab0e7f82fc1a39f87907343ca273223cc9a2f
Luko van der Maas <me@luko.dev>
2023-06-19 14:29:36 +0200

    add tailscale wireguard port

--- system/modules/networking.nix
+++ system/modules/networking.nix
@@ -1,12 +1,13 @@
-{config, ...}:
+{ config, ... }:
 {
   # Tailscale
   services.tailscale.enable = true;
 
   # Firewall
-  networking.firewall.allowedTCPPortRanges = [ { from = 1714; to = 1764; } { from = 8000; to = 8443; } { from = 27015; to = 27036; } ];
-  networking.firewall.allowedUDPPortRanges = [ { from = 1714; to = 1764; } { from = 27015; to = 27036; } { from = 10999; to = 10999; } { from=24642; to=24642; } ];
+  networking.firewall.allowedTCPPortRanges = [{ from = 1714; to = 1764; } { from = 8000; to = 8443; } { from = 27015; to = 27036; }];
+  networking.firewall.allowedUDPPortRanges = [{ from = 1714; to = 1764; } { from = 27015; to = 27036; } { from = 10999; to = 10999; } { from = 24642; to = 24642; }];
   networking.firewall.allowedTCPPorts = [ 8010 ]; # vlc chromecast
+  networking.firewall.allowedUDPPorts = [ 51820 ]; # tailscale
 
   networking.firewall.checkReversePath = "loose";
 
