cc970c4a450b55fa84934399888dbb4050abe727
Luko van der Maas <me@luko.dev>
2023-10-31 13:28:14 +0100

    add allow electron insecure

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   inputs = {
     nixpkgs.url = "nixpkgs/nixos-23.05";
-    nixpkgs-unstable.url = "nixpkgs/nixos-unstable"; 
+    nixpkgs-unstable.url = "nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
@@ -13,16 +13,20 @@
     };
   };
 
-  outputs = inputs: 
+  outputs = inputs:
     let
       system = "x86_64-linux";
       overlay-unstable = final: prev: {
         unstable = import inputs.nixpkgs-unstable {
           inherit system;
           config.allowUnfree = true;
+          config.permittedInsecurePackages = [
+            "electron-24.8.6"
+          ];
         };
       };
-    in {
+    in
+    {
       homeConfigurations = (
         import ./outputs/home-conf.nix {
           inherit inputs system overlay-unstable;
