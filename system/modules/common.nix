d09418e307ffac5b5c503dbdc69584b3df47a589
Luko van der Maas <me@luko.dev>
2023-10-31 13:16:10 +0100

    add gc

--- system/modules/common.nix
+++ system/modules/common.nix
@@ -18,6 +18,17 @@
       keep-outputs = true;
       keep-derivations = true;
     };
+
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
+
+    extraOptions = ''
+      min-free = ${toString (100 * 1024 * 1024)}
+      max-free = ${toString (1024 * 1024 * 1024)}
+    '';
   };
 
   nixpkgs.config.allowUnfree = true;
