d3cf0e73697575a123fd5a0184f983fcd3b9a0d1
Luko van der Maas <me@luko.dev>
2024-01-18 11:45:45 +0100

    update uris

--- system/modules/assist.nix
+++ system/modules/assist.nix
@@ -5,16 +5,19 @@
       enable = true;
       voice = "en_GB-semaine-medium";
       speaker = 3;
+      uri = "tcp://0.0.0.0:10200";
     };
 
     faster-whisper.servers.whisper = {
       enable = true;
       model = "medium-int8";
+      uri = "tcp://0.0.0.0:10300";
     };
 
     openwakeword = {
       enable = true;
       preloadModels = "hey_jarvis";
+      uri = "tcp://0.0.0.0:10500";
     };
   };
 
