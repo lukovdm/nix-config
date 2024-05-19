1644585d267aaa30b340aaae466f715b5d2a44c0
Luko van der Maas <me@luko.dev>
2024-05-19 19:41:49 +0200

    use docker for containers

--- system/modules/webcam.nix
+++ system/modules/webcam.nix
@@ -1,20 +1,25 @@
 { config, ... }: {
-  virtualisation.oci-containers.containers = {
-    restreamer = {
-      autoStart = true;
-      image = "datarhei/restreamer:latest";
-      ports = [ "8080:9000" ];
-      volumes = [
-        "/var/lib/restreamer/db:/restreamer/db"
-      ];
-      environment = {
-        "RS_USERNAME" = "admin";
-        "RS_PASSWORD" = "admin";
-        "RS_MODE" = "USBCAM";
+  virtualisation.docker.enable = true;
+
+  virtualisation.oci-containers = {
+    backend = "docker";
+    containers = {
+      restreamer = {
+        autoStart = true;
+        image = "datarhei/restreamer:latest";
+        ports = [ "8080:9000" ];
+        volumes = [
+          "/var/lib/restreamer/db:/restreamer/db"
+        ];
+        environment = {
+          "RS_USERNAME" = "admin";
+          "RS_PASSWORD" = "admin";
+          "RS_MODE" = "USBCAM";
+        };
+        extraOptions = [
+          "--device /dev/video0:/dev/video"
+        ];
       };
-      extraOptions = [
-        "--device /dev/video0:/dev/video"
-      ];
     };
   };
 
