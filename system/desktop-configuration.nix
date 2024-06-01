4cd1b720afa260d4ff41401e92996ac448e69551
Luko van der Maas <me@luko.dev>
2024-06-01 15:39:16 +0200

    update desktopy

--- system/desktop-configuration.nix
+++ system/desktop-configuration.nix
@@ -12,105 +12,107 @@
   # Display manager
   services.xserver = {
     enable = true;
+  };
 
-    desktopManager = {
-      plasma6 = {
-        enable = true;
-      };
-    };
-
-    displayManager = {
-      sddm = {
-        enable = true;
-        wayland.enable = true;
-      };
-    };
-
-    hardware.opengl.enable = true;
-
-    # Enable audio
-    security.rtkit.enable = true;
-    services.pipewire = {
+  services.desktopManager = {
+    plasma6 = {
       enable = true;
-      alsa.enable = true;
-      alsa.support32Bit = true;
-      pulse.enable = true;
     };
+  };
 
-    # Installed system packages
-    environment.systemPackages = with pkgs; [
-      firefox
-      xdg-desktop-portal
-      xorg.xkill
+  services.displayManager = {
+    sddm = {
+      enable = true;
+      wayland.enable = true;
+    };
+  };
 
-      kdePackages.kdeconnect-kde
-      kdePackages.krunner
-      kdePackages.sddm-kcm
-      kdePackages.ark
-      kdePackages.plasma-browser-integration
-      kdePackages.xdg-desktop-portal-kde
-      kde-gtk-config
+  hardware.opengl.enable = true;
+
+  # Enable audio
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  # Installed system packages
+  environment.systemPackages = with pkgs; [
+    firefox
+    xdg-desktop-portal
+    xorg.xkill
+
+    kdePackages.kdeconnect-kde
+    kdePackages.krunner
+    kdePackages.sddm-kcm
+    kdePackages.ark
+    kdePackages.plasma-browser-integration
+    kdePackages.xdg-desktop-portal-kde
+    kde-gtk-config
+  ];
+
+  # Printing
+  services.printing = {
+    enable = true;
+    drivers = with pkgs; [ gutenprint gutenprintBin hplip hplipWithPlugin ];
+    browsedConf = ''
+      BrowseRemoteProtocols none
+      BrowseLocalProtocols none
+      BrowseProtocols none
+      BrowseDeny All
+      BrowseInterval 100
+      BrowseTimeout 300
+      BrowsePoll nautilus.cosy.sbg.ac.at:443
+    '';
+  };
+  services.avahi.enable = true;
+  # Important to resolve .local domains of printers, otherwise you get an error
+  # like  "Impossible to connect to XXX.local: Name or service not known"
+  services.avahi.nssmdns = true;
+
+  # Enable steam
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+    dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  };
+  hardware.steam-hardware.enable = true;
+  services.joycond.enable = true;
+
+  programs._1password-gui = {
+    enable = true;
+    polkitPolicyOwners = [ "luko" ];
+  };
+
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "qt";
+  };
+  services.pcscd.enable = true;
+
+  # Add fonts
+  fonts = {
+    packages = with pkgs; [
+      (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "SourceCodePro" "Hack" ]; })
+      noto-fonts
+      font-awesome
+      roboto-mono
+      dejavu_fonts
+      xkcd-font
     ];
+  };
 
-    # Printing
-    services.printing = {
-      enable = true;
-      drivers = with pkgs; [ gutenprint gutenprintBin hplip hplipWithPlugin ];
-      browsedConf = ''
-        BrowseRemoteProtocols none
-        BrowseLocalProtocols none
-        BrowseProtocols none
-        BrowseDeny All
-        BrowseInterval 100
-        BrowseTimeout 300
-        BrowsePoll nautilus.cosy.sbg.ac.at:443
-      '';
-    };
-    services.avahi.enable = true;
-    # Important to resolve .local domains of printers, otherwise you get an error
-    # like  "Impossible to connect to XXX.local: Name or service not known"
-    services.avahi.nssmdns = true;
+  # Enable docker
+  virtualisation.docker.enable = true;
 
-    # Enable steam
-    programs.steam = {
-      enable = true;
-      remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-      dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-    };
-    hardware.steam-hardware.enable = true;
-    services.joycond.enable = true;
+  # PlatformIO
+  services.udev.packages = [ pkgs.platformio ];
 
-    programs._1password-gui = {
-      enable = true;
-      polkitPolicyOwners = [ "luko" ];
-    };
+  # System version, do not update here!
+  system.stateVersion = "22.05";
 
-    programs.gnupg.agent = {
-      enable = true;
-      enableSSHSupport = true;
-      pinentryFlavor = "qt";
-    };
-    services.pcscd.enable = true;
+}
 
-    # Add fonts
-    fonts = {
-      packages = with pkgs; [
-        (nerdfonts.override { fonts = [ "FiraCode" "DroidSansMono" "SourceCodePro" "Hack" ]; })
-        noto-fonts
-        font-awesome
-        roboto-mono
-        dejavu_fonts
-        xkcd-font
-      ];
-    };
-
-    # Enable docker
-    virtualisation.docker.enable = true;
-
-    # PlatformIO
-    services.udev.packages = [ pkgs.platformio ];
-
-    # System version, do not update here!
-    system.stateVersion = "22.05";
-
-  }
