02c21dccb648861dbbfd34bf9b5cab99aaa9ae72
Luko van der Maas <me@luko.dev>
2024-12-01 14:13:53 +0100

    possible jellyfin fix

--- system/hardware-configurations/krypton.nix
+++ system/hardware-configurations/krypton.nix
@@ -40,6 +40,10 @@
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
+  hardware.graphics = {
+    enable = true;
+  };
+
   hardware.nvidia.modesetting.enable = true;
   hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
   hardware.opengl = {
