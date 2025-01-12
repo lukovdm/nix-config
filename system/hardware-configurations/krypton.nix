b97379ac15b3c665de5c68d2d2f82d5fbdcaa08f
Luko van der Maas <me@luko.dev>
2025-01-12 11:01:09 +0100

    change nividia driver version

--- system/hardware-configurations/krypton.nix
+++ system/hardware-configurations/krypton.nix
@@ -41,7 +41,7 @@
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
   hardware.nvidia.modesetting.enable = true;
-  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.stable;
+  hardware.nvidia.package = config.boot.kernelPackages.nvidiaPackages.beta;
   hardware.nvidia.open = false;
   hardware.graphics = {
     enable = true;
