56337ed9b3e35dc59ad00b5d46d51d36402234e3
Luko van der Maas <me@luko.dev>
2026-02-21 14:04:22 +0100

    update bgg-mm

--- system/modules/bbg-mm.nix
+++ system/modules/bbg-mm.nix
@@ -2,30 +2,16 @@
 {
   age.secrets.bbg-mm-token = {
     file = ../../secrets/bbg-mm-token.age;
-    owner = "bggmm";
+    owner = "bgg-mm";
   };
-
-  users.users.bggmm = {
-    isSystemUser = true;
-    group = "bggmm";
-    home = "/var/lib/bgg-mm";
-    createHome = true;
-  };
-  users.groups.bggmm = { };
 
   services.bgg-mm = {
     enable = true;
-    user = "bggmm";
     schedule = "0 8 * * *";
     tokenFile = config.age.secrets.bbg-mm-token.path;
-    stateFile = "/var/lib/bgg-mm/availability.json";
 
     bgg = {
       username = "mageleve";
-    };
-
-    shop = {
-      baseUrl = "http://www.moenen-en-mariken.nl";
     };
 
     ntfy = {
