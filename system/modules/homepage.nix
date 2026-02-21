0c55d1c6e7ba136710bb4bbd9122684c37e500f7
Luko van der Maas <me@luko.dev>
2026-02-21 13:05:32 +0100

    fix homepage

--- system/modules/homepage.nix
+++ system/modules/homepage.nix
@@ -43,142 +43,148 @@ in
     ];
 
     # Services: each group only appears if the relevant module is enabled.
-    services = lib.filter (g: g != null) [
-
-      (lib.mkIf cfg.jellyfin.enable {
-        Media = [
-          {
-            Jellyfin = {
-              href = "http://krypton:8096";
-              description = "Media server";
-              icon = "jellyfin.svg";
-              widget = {
-                type = "jellyfin";
-                url = "http://localhost:8096";
-                key = "{{HOMEPAGE_VAR_JELLYFIN_KEY}}";
-                enableBlocks = true;
+    services =
+      lib.optionals cfg.jellyfin.enable [
+        {
+          Media = [
+            {
+              Jellyfin = {
+                href = "http://krypton:8096";
+                description = "Media server";
+                icon = "jellyfin.svg";
+                widget = {
+                  type = "jellyfin";
+                  url = "http://localhost:8096";
+                  key = "{{HOMEPAGE_VAR_JELLYFIN_KEY}}";
+                  enableBlocks = true;
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.radarr.enable {
-        Media = [
-          {
-            Radarr = {
-              href = "http://krypton:7878";
-              description = "Movie management";
-              icon = "radarr.svg";
-              widget = {
-                type = "radarr";
-                url = "http://localhost:7878";
-                key = "{{HOMEPAGE_VAR_RADARR_KEY}}";
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.radarr.enable [
+        {
+          Media = [
+            {
+              Radarr = {
+                href = "http://krypton:7878";
+                description = "Movie management";
+                icon = "radarr.svg";
+                widget = {
+                  type = "radarr";
+                  url = "http://localhost:7878";
+                  key = "{{HOMEPAGE_VAR_RADARR_KEY}}";
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.sonarr.enable {
-        Media = [
-          {
-            Sonarr = {
-              href = "http://krypton:8989";
-              description = "TV show management";
-              icon = "sonarr.svg";
-              widget = {
-                type = "sonarr";
-                url = "http://localhost:8989";
-                key = "{{HOMEPAGE_VAR_SONARR_KEY}}";
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.sonarr.enable [
+        {
+          Media = [
+            {
+              Sonarr = {
+                href = "http://krypton:8989";
+                description = "TV show management";
+                icon = "sonarr.svg";
+                widget = {
+                  type = "sonarr";
+                  url = "http://localhost:8989";
+                  key = "{{HOMEPAGE_VAR_SONARR_KEY}}";
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.transmission.enable {
-        "Downloads & Indexing" = [
-          {
-            Transmission = {
-              href = "http://krypton:9091";
-              description = "Torrent client";
-              icon = "transmission.svg";
-              widget = {
-                type = "transmission";
-                url = "http://localhost:9091";
-                username = "{{HOMEPAGE_VAR_TRANSMISSION_USER}}";
-                password = "{{HOMEPAGE_VAR_TRANSMISSION_PASS}}";
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.transmission.enable [
+        {
+          "Downloads & Indexing" = [
+            {
+              Transmission = {
+                href = "http://krypton:9091";
+                description = "Torrent client";
+                icon = "transmission.svg";
+                widget = {
+                  type = "transmission";
+                  url = "http://localhost:9091";
+                  username = "{{HOMEPAGE_VAR_TRANSMISSION_USER}}";
+                  password = "{{HOMEPAGE_VAR_TRANSMISSION_PASS}}";
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.prowlarr.enable {
-        "Downloads & Indexing" = [
-          {
-            Prowlarr = {
-              href = "http://krypton:9696";
-              description = "Indexer manager";
-              icon = "prowlarr.svg";
-              widget = {
-                type = "prowlarr";
-                url = "http://localhost:9696";
-                key = "{{HOMEPAGE_VAR_PROWLARR_KEY}}";
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.prowlarr.enable [
+        {
+          "Downloads & Indexing" = [
+            {
+              Prowlarr = {
+                href = "http://krypton:9696";
+                description = "Indexer manager";
+                icon = "prowlarr.svg";
+                widget = {
+                  type = "prowlarr";
+                  url = "http://localhost:9696";
+                  key = "{{HOMEPAGE_VAR_PROWLARR_KEY}}";
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.bazarr.enable {
-        "Downloads & Indexing" = [
-          {
-            Bazarr = {
-              href = "http://krypton:6767";
-              description = "Subtitle manager";
-              icon = "bazarr.svg";
-              widget = {
-                type = "bazarr";
-                url = "http://localhost:6767";
-                key = "{{HOMEPAGE_VAR_BAZARR_KEY}}";
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.bazarr.enable [
+        {
+          "Downloads & Indexing" = [
+            {
+              Bazarr = {
+                href = "http://krypton:6767";
+                description = "Subtitle manager";
+                icon = "bazarr.svg";
+                widget = {
+                  type = "bazarr";
+                  url = "http://localhost:6767";
+                  key = "{{HOMEPAGE_VAR_BAZARR_KEY}}";
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.photoprism.enable {
-        Photos = [
-          {
-            Photoprism = {
-              href = "http://krypton:2342";
-              description = "Photo library";
-              icon = "photoprism.svg";
-              widget = {
-                type = "photoprism";
-                url = "http://localhost:2342";
-                username = "{{HOMEPAGE_VAR_PHOTOPRISM_USER}}";
-                password = "{{HOMEPAGE_VAR_PHOTOPRISM_PASS}}";
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.photoprism.enable [
+        {
+          Photos = [
+            {
+              Photoprism = {
+                href = "http://krypton:2342";
+                description = "Photo library";
+                icon = "photoprism.svg";
+                widget = {
+                  type = "photoprism";
+                  url = "http://localhost:2342";
+                  username = "{{HOMEPAGE_VAR_PHOTOPRISM_USER}}";
+                  password = "{{HOMEPAGE_VAR_PHOTOPRISM_PASS}}";
+                };
               };
-            };
-          }
-        ];
-      })
-
-      (lib.mkIf cfg.mattermost.enable {
-        Comms = [
-          {
-            Mattermost = {
-              href = "https://mm.luko.dev";
-              description = "Team chat";
-              icon = "mattermost.svg";
-            };
-          }
-        ];
-      })
-
-    ];
+            }
+          ];
+        }
+      ]
+      ++ lib.optionals cfg.mattermost.enable [
+        {
+          Comms = [
+            {
+              Mattermost = {
+                href = "https://mm.luko.dev";
+                description = "Team chat";
+                icon = "mattermost.svg";
+              };
+            }
+          ];
+        }
+      ];
   };
 }
